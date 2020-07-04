934a0e742b34f104501dfbec110ed9237b4aa0e4
Michael Brian Puckett, II <michael_puckett_ii@live.com>
2020-07-04 12:05:03 -0400

    Added custom boolean test.

--- IFTTT.Tests/ConditionalExpressions/BooleanExpressionTests.cs
+++ IFTTT.Tests/ConditionalExpressions/BooleanExpressionTests.cs
@@ -1,6 +1,8 @@
 ﻿using IFTTT.Enums;
+using IFTTT.Expressions;
 using IFTTT.Expressions.ConditionalExpressions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 
 namespace IFTTT.Tests.ConditionalExpressions
 {
@@ -23,6 +25,46 @@ namespace IFTTT.Tests.ConditionalExpressions
 
             //Assert
             Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void TestCustomBooleanExpression()
+        {
+            //Arrange
+            var name = "Michael";
+            var age = 40;
+
+            var minimumLengthTest = new Predicate<string>((x) => x.Length > 6);
+            var minimumAgeTest = new Predicate<int>((x) => x > 21);
+
+            var actor = new FuncBooleanTest
+            {
+                AValue = name,
+                Operator = ConditionalOperator.And,
+                BValue = age,
+                A = minimumLengthTest,
+                B = minimumAgeTest
+            };
+
+            //Act
+            var result = actor.Result();
+
+            //Assert
+            Assert.IsTrue(result);
+        }
+
+        class FuncBooleanTest : IExpression<Predicate<string>, ConditionalOperator, Predicate<int>>, IExpressionResult<bool>
+        {
+            public string AValue { get; set; }
+            public int BValue { get; set; }
+            public Predicate<string> A { get; set; }
+            public Predicate<int> B { get; set; }
+            public ConditionalOperator Operator { get; set; }
+            public bool Result() => Operator switch
+            {
+                ConditionalOperator.Or => A.Invoke(AValue) || B.Invoke(BValue),
+                _ => A.Invoke(AValue) && B.Invoke(BValue)
+            };
         }
     }
 }
